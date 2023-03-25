6ee361bce3a735a243f5652e294a180206ede3e5
neprostoilya <geniumobile@gmail.com>
2023-03-26 01:07:41 +0500

    HW_10

--- lesson 10/main.py
+++ lesson 10/main.py
@@ -1,3 +1,5 @@
+
+
 # Задание №1
 # В примере найти и вывести трехзначные числа с помощью регулярных выражений.
 
@@ -18,3 +20,90 @@ collors = ['#ABCDEF', '#54#', '#F08080', '#FA8072', 'fghw3d', '#8B0000']
 print(re.findall(r'#\S{6}', str(collors)))
 '''
 
+
+# Задание №3
+# import re
+# text = ['Завтрак в 09:00', 'Завтрак в 90:00', 'Обед  в 13:00', 'Обед в 13:61', 'Ужин в 19:05', 'Ужин в 37:98', 'Ужин в 24:01']
+
+
+
+# Задание №4
+# Создать запрос для выбора из текста дробных чисел с разделителем дробной части в виде точки.
+# Разряды целой части могут не выделяться или отделяться пробелом или запятой. 1231.12313
+
+'''
+import re
+numbers = [1231.12313, 2121.121, 3.14, 6598, 9898787, 999.99, '098 90', '123,123']
+print(re.findall(r'\d+[.]\d+', str(numbers)))
+'''
+
+
+
+# Задание №6
+# С сайта https://jsonplaceholder.typicode.com/
+
+
+# a) На основе WEB ресурса Создать свои JSON файлы
+
+'''
+import json
+import requests
+
+todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+listok = []
+
+for todo in todos:
+    UserId = todo['userId']
+    Id = todo['id']
+    Title = todo['title']
+    Completed = todo['completed']
+
+    listok.append({
+        'UserID': UserId,
+        'ID' : Id,
+        'Title' : Title,
+        'Completed' : Completed
+    })
+
+with open('lesson 10/files/json and csv files/todos.json', mode='w', encoding='UTF-8') as file:
+    json.dump(listok, file, ensure_ascii=False, indent=4)
+'''
+
+# b) c JSON файлов переделать данные в формат CSV файла
+
+'''
+import json
+import csv
+
+with open('lesson 10/files/json and csv files/todos.json', encoding="UTF-8") as file:
+    src = json.load(file)
+
+with open('lesson 10/files/json and csv files/todos_to_csv.csv', mode='w', encoding="UTF-8") as file2:
+    result = csv.DictWriter(file2, fieldnames="UserID ID Title Completed".split())
+    result.writeheader()
+
+
+    for line in src:
+        result.writerow(line)
+'''
+
+# c)
+
+'''
+import json
+import csv
+
+data_Json = []
+
+with open('lesson 10/files/json and csv files/todos_to_csv.csv') as file:
+    reader_csv = csv.DictReader(file)
+
+
+    for line in reader_csv:
+        data_Json.append(line)
+
+
+with open('lesson 10/files/json and csv files/csv_to_json.csv', mode='w', encoding="UTF-8") as file2:
+    json.dump(data_Json, file2, ensure_ascii=False, indent=4)
+'''
