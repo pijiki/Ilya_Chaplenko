0db33a4962cc24199106e25596032ccba26ba1b3
neprostoilya <geniumobile@gmail.com>
2023-04-09 16:34:30 +0500

    HW_12

--- lesson 12/api_countryinfo.py
+++ lesson 12/api_countryinfo.py
@@ -2,12 +2,12 @@ from countryinfo import CountryInfo as Info
 while True:
     country = Info(input('Enter country: ')).info()
     print(f'''
-Name : {country['name']}
-Capital : {country['capital']}
-Region : {country['region']}   
-Population : {country['population']}
-Language : {country['languages']}
-TimeZones : {country['timezones']}
-Area : {country['area']} km²
-Wiki : {country['wiki']}
+Name : \033[1m{country['name']}\033[0m
+Capital : \033[33m{country['capital']}\033[0m
+Region : \033[33m{country['region']}\033[0m  
+Population : \033[33m{country['population']}\033[0m
+Language : \033[33m{country['languages']}\033[0m
+TimeZones : \033[33m{country['timezones']}\033[0m
+Area : \033[33m{country['area']} km²\033[0m
+Wiki : \033[33m{country['wiki']}\033[0m
 ''')
