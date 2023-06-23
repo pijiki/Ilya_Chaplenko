5d0068ede4e5342c615dbffdd7971a871743db76
neprostoilya <geniumobile@gmail.com>
2023-06-24 02:42:04 +0500

    HW_22

--- lesson 19_20_21_22/Cooking_Web/urls.py
+++ lesson 19_20_21_22/Cooking_Web/urls.py
@@ -4,5 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('category/<int:pk>/', category_list, name='category_list'),
-    path('post/<int:pk>/', article_detail, name='article_detail')
+    path('post/<int:pk>/', article_detail, name='article_detail'),
+    path('add_article/', add_post, name='add'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
 ]
