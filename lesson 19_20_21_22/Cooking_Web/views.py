5bd886bab4b61c5fbe7c8a95b5fd1a0da84e2dc3
neprostoilya <geniumobile@gmail.com>
2023-06-24 02:43:30 +0500

    HW_22

--- lesson 19_20_21_22/Cooking_Web/views.py
+++ lesson 19_20_21_22/Cooking_Web/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from .models import Post, Category
+from django.shortcuts import render, redirect
+from .forms import PostAddForm, LoginForm, RegisterForm
+from .models import Post
 from django.db.models import F
+from django.contrib.auth import login, logout
+from django.contrib import messages
+
+
 
 def index(request):
     """Главная страница"""
@@ -10,7 +15,11 @@ def index(request):
         'title': 'Главная страница',
         'posts': posts
     }
-    return render(request, 'Cooking_Web/index.html', context)
+    return render(
+        request, 
+        'Cooking_Web/index.html', 
+        context
+    )
 
 def category_list(request, pk):
     """Реакция на нажатие кнопки"""
@@ -20,7 +29,11 @@ def category_list(request, pk):
         'title': posts[0].category.title if posts else 'Статей на данной категории не существует!',
         'posts': posts
     }
-    return render(request, 'Cooking_web/index.html', context)
+    return render(
+        request, 
+        'Cooking_web/index.html', 
+        context
+    )
 
 def article_detail(request, pk):
     """Страница статьи"""
@@ -34,4 +47,71 @@ def article_detail(request, pk):
         'recommendations': recommendations
      }
 
-    return render(request, 'Cooking_web/article_detail.html', context)
+    return render(
+        request, 
+        'Cooking_web/article_detail.html',
+        context
+    )
+
+def add_post(request):
+    """Добавление статьи от юзера"""
+    if request.method == 'POST':
+        form = PostAddForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = Post.objects.create(**form.cleaned_data)
+            post.save()
+            return redirect('article_detail', post.pk)
+    else:
+        form = PostAddForm()
+    
+    context = {
+        'form': form,
+        'title': "Добавить статью"
+    }
+    return render(
+        request, 
+        'Cooking_web/article_add_form.html', 
+        context
+    )
+
+def user_login(request):
+    """Аутендификация юзера"""
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Вы успешно вошли в аккаунт')
+            return redirect('index')
+    else:
+        form = LoginForm()
+
+    context = {
+        'title': 'Авторизация пользователя',
+        'form': form
+    }
+
+    return render(request, 'Cooking_web/login_form.html', context)
+
+
+def user_logout(request):
+    """Выход пользователя"""
+    logout(request)
+    return redirect('index')
+
+def register(request):
+    """Регистрация юзера"""
+    if request.method == 'POST':
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else: 
+        form = RegisterForm()
+    
+    context = {
+        'title': 'Регистрация пользователя',
+        'form': form
+    }
+    return render(request, 'Cooking_web/register.html', context)
